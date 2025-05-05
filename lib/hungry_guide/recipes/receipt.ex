a8a21f3f9cf083284ff376d598c0bced2444f473
ppp3ppj <ppp.3ppj@gmail.com>
2025-05-05 19:47:28 +0700

    add: delete all for if delete receipt will delete receipt_ingredients too.

--- lib/hungry_guide/recipes/receipt.ex
+++ lib/hungry_guide/recipes/receipt.ex
@@ -9,7 +9,10 @@ defmodule HungryGuide.Recipes.Receipt do
     field :name, :string
     field :description, :string
 
-    has_many :receipt_ingredients, Recipes.ReceiptIngredient, on_replace: :delete
+    has_many :receipt_ingredients, Recipes.ReceiptIngredient,
+      on_replace: :delete,
+      on_delete: :delete_all
+
     has_many :ingredients, through: [:receipt_ingredients, :ingredient]
 
     timestamps(type: :utc_datetime)
