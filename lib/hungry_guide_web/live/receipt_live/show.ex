a11859a15c382d56541820a00197b8bb73cb113d
ppp3ppj <ppp.3ppj@gmail.com>
2025-05-07 19:38:30 +0700

    fix: edit receipt when click via show receipt

--- lib/hungry_guide_web/live/receipt_live/show.ex
+++ lib/hungry_guide_web/live/receipt_live/show.ex
@@ -1,6 +1,7 @@
 defmodule HungryGuideWeb.ReceiptLive.Show do
   use HungryGuideWeb, :live_view
 
+  alias HungryGuide.Inventories
   alias HungryGuide.Recipes
 
   @impl true
@@ -12,12 +13,23 @@ defmodule HungryGuideWeb.ReceiptLive.Show do
   def handle_params(%{"id" => id}, _, socket) do
     receipt = Recipes.get_receipt!(id)
     receipt_ingredients = Recipes.get_receipt_ingredients(id)
+    ingredients = Inventories.list_ingredients()
     IO.inspect(receipt_ingredients)
+
+    initial_quantities = Map.new(ingredients, fn ingr -> {ingr.id, 0} end)
+
+    # Merge actual quantities from receipt_ingredients
+    quantities =
+      Enum.reduce(receipt_ingredients, initial_quantities, fn ri, acc ->
+        Map.put(acc, ri.ingredient_id, ri.quantity)
+      end)
 
     {:noreply,
      socket
      |> assign(:page_title, page_title(socket.assigns.live_action))
      |> assign(:receipt, receipt)
+     |> assign(:ingredients, ingredients)
+     |> assign(:quantities, quantities)
      |> assign(:receipt_ingredients, receipt_ingredients)}
   end
 
