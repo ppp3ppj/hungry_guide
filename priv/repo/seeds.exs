3709d7e963f9aaff96e9d77f821b6948b96b0cb6
ppp3ppj <ppp.3ppj@gmail.com>
2025-05-10 14:18:35 +0700

    add: admin seed logic

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,22 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias HungryGuide.Accounts
+admin_email = "admin@admin.com"
+admin_password = "supersecret123"
+admin_name = "admin"
+
+case Accounts.get_user_by_email(admin_email) do
+  nil ->
+    {:ok, _admin} = Accounts.register_user(%{
+      name: admin_name,
+      email: admin_email,
+      password: admin_password
+    })
+
+    IO.puts("Default admin user created: #{admin_email}")
+
+  _user ->
+    IO.puts("Admin user already exists: #{admin_email}")
+end
