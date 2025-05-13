b8c07fdb3ce4f03d9d1d0c062c27af092c91f87f
ppp3ppj <ppp.3ppj@gmail.com>
2025-05-13 19:42:50 +0700

    refactor: change theme button like daisyui button in preview page

--- lib/hungry_guide_web/components/core_components.ex
+++ lib/hungry_guide_web/components/core_components.ex
@@ -696,11 +696,32 @@ defmodule HungryGuideWeb.CoreComponents do
       class={["dropdown dropdown-bottom dropdown-end", @class]}
     >
       <!-- Desktop Icon -->
-      <div tabindex="0" role="button" class="btn btn-ghost hidden md:flex">
+      <div tabindex="0" role="button" class="btn group btn-sm gap-1.5 px-1.5 btn-ghost hidden md:flex">
         <!--
         {@label}
         -->
+        <div class="bg-base-100 group-hover:border-base-content/20 border-base-content/10 grid shrink-0 grid-cols-2 gap-0.5 rounded-md border p-1 transition-colors">
+          <div class="bg-base-content size-1 rounded-full"></div>
+
+          <div class="bg-primary size-1 rounded-full"></div>
+
+          <div class="bg-secondary size-1 rounded-full"></div>
+
+          <div class="bg-accent size-1 rounded-full"></div>
+        </div>
+        <svg
+          width="12px"
+          height="12px"
+          class="mt-px hidden size-2 fill-current opacity-60 sm:inline-block"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 2048 2048"
+        >
+          <path d="M1799 349l242 241-1017 1017L7 590l242-241 775 775 775-775z"></path>
+        </svg>
+
+    <!--
         <.icon name="hero-sparkles-solid" class="h-4 w-4" />
+        -->
         <!--{@label || "Theme"}
         <Heroicons.chevron_down class="h-3 w-3" />
 
